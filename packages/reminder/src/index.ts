30a98059987e6b97f1cc4982a43d01d5074fea30
Ryan Martin <54093748+eqpoqpe@users.noreply.github.com>
2023-10-30 22:59:11 +0800

    Update index.ts

---
+++ packages/reminder/src/index.ts
@@ -0,0 +1,24 @@
+/**
+ * set a reminder function that triggers based on the callback's return value.
+ * @param timeInMilliseconds - time in milliseconds for when the reminder will trigger.
+ * @param callback - reminder callback function that can return boolean or void.
+ * @returns returns a setTimeout reference for manual reminder cancellation.
+ */
+function setReminder(timeInMilliseconds: number, callback: ReminderCallback): ReturnType<typeof setTimeout> {
+  const reminderTimeout = setTimeout(function() {
+    const result = callback(reminderTimeout);
+    if (result === true) {
+      setReminder(timeInMilliseconds, callback);
+    }
+  }, timeInMilliseconds);
+
+  return reminderTimeout;
+}
+
+/**
+ * stop a reminder function by canceling the reminder set using setTimeout.
+ * @param reminderTimeout - setTimeout reference to identify the reminder to cancel.
+ */
+function offReminder(reminderTimeout: ReturnType<typeof setTimeout>) {
+  clearTimeout(reminderTimeout);
+}
