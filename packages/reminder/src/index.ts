7c328de6570625a600207cd61e1d34714ed405cf
Ryan Martin <54093748+eqpoqpe@users.noreply.github.com>
2023-11-20 11:06:05 +0000

    fix reminder

--- packages/reminder/src/index.ts
+++ packages/reminder/src/index.ts
@@ -1,16 +1,23 @@
+type Reminder = ReturnType<typeof setTimeout>;
+type ReminderCallback = (offReminder: (reminder: Reminder) => void) => boolean;
+
 /**
  * set a reminder function that triggers based on the callback's return value.
  * @param timeInMilliseconds - time in milliseconds for when the reminder will trigger.
  * @param callback - reminder callback function that can return boolean or void.
  * @returns returns a setTimeout reference for manual reminder cancellation.
  */
-function setReminder(timeInMilliseconds: number, callback: ReminderCallback): ReturnType<typeof setTimeout> {
-  const reminderTimeout = setTimeout(function() {
-    const result = callback(reminderTimeout);
-    if (result === true) {
-      setReminder(timeInMilliseconds, callback);
-    } else {
+function setReminder(
+  timeInMilliseconds: number,
+  callback: ReminderCallback
+): Reminder {
+  const reminderTimeout = setTimeout(function () {
+    const result = callback(() => offReminder(reminderTimeout));
+
+    if (result) {
       offReminder(reminderTimeout);
+    } else {
+      setReminder(timeInMilliseconds, callback);
     }
   }, timeInMilliseconds);
 
@@ -21,7 +28,7 @@ function setReminder(timeInMilliseconds: number, callback: ReminderCallback): Re
  * stop a reminder function by canceling the reminder set using setTimeout.
  * @param reminderTimeout - setTimeout reference to identify the reminder to cancel.
  */
-function offReminder(reminderTimeout: ReturnType<typeof setTimeout>) {
+function offReminder(reminderTimeout: Reminder) {
   clearTimeout(reminderTimeout);
 }
 
