506d591d86c572637eb83c188029339f5f9f4e0f
Ryan Martin <eqpoqpe@gmail.com>
2025-05-16 19:27:50 +0800

    Update version to 0.1.1 in package.json and refactor declaringDestructor function for improved readability

--- packages/react/src/declaring-destructor.ts
+++ packages/react/src/declaring-destructor.ts
@@ -1,25 +1,24 @@
 import { Children, FC, ReactElement, ReactNode, isValidElement } from "react";
 
+type ElementObjectMap<T> = { [K in keyof T]: ReactElement[] };
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function declaringDestructor<T extends Record<string, FC<any>>>(
   children: ReactNode,
   targets: T
-): { [K in keyof T]: ReactElement[] } {
+): ElementObjectMap<T> {
   const keys = Object.keys(targets) as Array<keyof T>;
-  const elementMap: { [K in keyof T]: ReactElement[] } = {} as {
-    [K in keyof T]: ReactElement[];
-  };
+  const elementMap = keys.reduce((acc, key) => {
+    acc[key] = [];
+    return acc;
+  }, {} as ElementObjectMap<T>);
 
-  keys.forEach((key) => {
-    elementMap[key] = [];
-  });
-
-  Children.map(children, (child) => {
+  Children.forEach(children, (child) => {
     if (isValidElement(child)) {
-      const targetKey = keys.find((key) => child.type === targets[key]);
+      const matchedKey = keys.find((key) => child.type === targets[key]);
 
-      if (targetKey) {
-        elementMap[targetKey].push(child);
+      if (matchedKey) {
+        elementMap[matchedKey].push(child);
       }
     }
   });
